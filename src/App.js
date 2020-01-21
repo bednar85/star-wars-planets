c259a4f1748f29094331d8869951b89ef8b18f8b
Ryan <bednar85@gmail.com>
2020-01-21 00:29:30 -0500

    overhaul filter logic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.scss';
@@ -44,8 +17,7 @@ class App extends Component {
 
     this.state = {
       filters: {
-        // title: [],
-        media: [],
+        media: 'All',
         era: []
       }
     };
@@ -56,17 +28,27 @@ class App extends Component {
 
     const [key, value] = event.target.value.split(':');
 
-    const targetFilterGroup = filters[key];
+    // console.log('');
+    // console.log('key:', key);
+    // console.log('value:', value);
 
-    /**
-     * if the filter already exists in the targetFilterGroup, remove it with .filter
-     * else use the spread operator, create a copy of the targetFilterGroup and append the new filter to the end
-     */
-    const updatedFilterGroup = targetFilterGroup.includes(value)
-      ? targetFilterGroup.filter(filter => filter !== value)
-      : [...targetFilterGroup, value];
+    let newFilterValue;
 
-    const updatedFilters = { ...filters, [key]: updatedFilterGroup };
+    if (key === 'era') {
+      const targetFilterGroup = filters.era;
+
+      /**
+       * if the filter already exists in the targetFilterGroup, remove it with .filter
+       * else use the spread operator, create a copy of the targetFilterGroup and append the new filter to the end
+       */
+      newFilterValue = targetFilterGroup.includes(value)
+        ? targetFilterGroup.filter(filter => filter !== value)
+        : [...targetFilterGroup, value];
+    } else {
+      newFilterValue = value;
+    }
+
+    const updatedFilters = { ...filters, [key]: newFilterValue };
 
     this.setState({
       filters: updatedFilters
@@ -83,48 +65,42 @@ class App extends Component {
     const activeFilterKeys = Object.entries(filters).reduce((acc, curr) => {
       const [key, value] = curr;
 
-      if (value.length) {
+      /**
+       * if key is media and value is not All
+       * OR
+       * if key is era and value array has a length
+       * add filter key to accumulator
+       */
+      if (
+        (key === 'media' && value !== 'All') ||
+        (key === 'era' && value.length)
+      ) {
         acc.push(key);
       }
 
       return acc;
     }, []);
 
-    // console.log('activeFilterKeys:', activeFilterKeys);
-
-    if (activeFilterKeys.length) {
+    // if both filters are applied check both conditions
+    if (activeFilterKeys.length > 1) {
       return planets.filter(planet => {
-        // console.log('activeFilterKeys:', activeFilterKeys);
+        const matchingAppearances = planet.appearances
+          .filter(appearance => appearance.media === filters.media)
+          .map(appearance => appearance.era);
 
-        const conditions = {};
+        return findAny(filters.era, matchingAppearances);
+      });
+    }
 
-        // for each active filter key
+    // if only one filter is applied
+    if (activeFilterKeys.length === 1) {
+      return planets.filter(planet => {
+        const activeFilterKey = activeFilterKeys[0];
+        const planetValues = planet.appearances.map(p => p[activeFilterKey]);
 
-        // // check each condition
-        activeFilterKeys.forEach(currentFilterKey => {
-          const planetValues = planet.appearances.map(p => p[currentFilterKey]);
-          const filterValues = filters[currentFilterKey];
-
-          // console.log('planetValues:', planetValues);
-          // console.log('filterValues:', filterValues);
-
-          // // console.log(planetValues.some(d => d. === 0));
-
-          // console.log(
-          //   'contains(planetValues, filterValues):',
-          //   contains(planetValues, filterValues)
-          // );
-
-          // console.log('planetValues.includes(filterValue):', planetValues.includes(filterValue));
-          conditions[currentFilterKey] = findAny(filterValues, planetValues);
-        });
-
-        // console.log('conditions:', conditions);
-        // console.log('');
-
-        return Object.values(conditions).every(
-          currentValue => currentValue === true
-        );
+        return activeFilterKey === 'era'
+          ? findAny(filters.era, planetValues)
+          : planetValues.includes(filters.media);
       });
     }
 
@@ -191,25 +167,38 @@ class App extends Component {
             <h2 className="filter-form-heading">Media:</h2>
             <div className="filter-form-input-wrapper">
               <input
-                type="checkbox"
+                type="radio"
                 className="filter-form-input"
-                id="film"
-                name="film"
+                id="media-all"
+                name="media"
+                value="media:All"
+                defaultChecked
+              />
+              <label className="filter-form-label" htmlFor="media-all">
+                All
+              </label>
+            </div>
+            <div className="filter-form-input-wrapper">
+              <input
+                type="radio"
+                className="filter-form-input"
+                id="media-film"
+                name="media"
                 value="media:Film"
               />
-              <label className="filter-form-label" htmlFor="film">
+              <label className="filter-form-label" htmlFor="media-film">
                 Film
               </label>
             </div>
             <div className="filter-form-input-wrapper">
               <input
-                type="checkbox"
+                type="radio"
                 className="filter-form-input"
-                id="tv"
-                name="tv"
+                id="media-tv"
+                name="media"
                 value="media:TV Series"
               />
-              <label className="filter-form-label" htmlFor="tv">
+              <label className="filter-form-label" htmlFor="media-tv">
                 TV Series
               </label>
             </div>
@@ -220,11 +209,14 @@ class App extends Component {
               <input
                 type="checkbox"
                 className="filter-form-input"
-                id="prequel-trilogy"
-                name="prequel-trilogy"
+                id="era-prequel-trilogy"
+                name="era"
                 value="era:Prequel Trilogy"
               />
-              <label className="filter-form-label" htmlFor="prequel-trilogy">
+              <label
+                className="filter-form-label"
+                htmlFor="era-prequel-trilogy"
+              >
                 Prequel Trilogy
               </label>
             </div>
@@ -232,11 +224,14 @@ class App extends Component {
               <input
                 type="checkbox"
                 className="filter-form-input"
-                id="original-trilogy"
-                name="original-trilogy"
+                id="era-original-trilogy"
+                name="era"
                 value="era:Original Trilogy"
               />
-              <label className="filter-form-label" htmlFor="original-trilogy">
+              <label
+                className="filter-form-label"
+                htmlFor="era-original-trilogy"
+              >
                 Original Trilogy
               </label>
             </div>
@@ -244,11 +239,11 @@ class App extends Component {
               <input
                 type="checkbox"
                 className="filter-form-input"
-                id="sequel-trilogy"
-                name="sequel-trilogy"
+                id="era-sequel-trilogy"
+                name="era"
                 value="era:Sequel Trilogy"
               />
-              <label className="filter-form-label" htmlFor="sequel-trilogy">
+              <label className="filter-form-label" htmlFor="era-sequel-trilogy">
                 Sequel Trilogy
               </label>
             </div>
