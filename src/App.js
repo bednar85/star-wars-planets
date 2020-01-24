c4f3b5e9c20c62174482ccb9ec9d351a95297fb3
Ryan <bednar85@gmail.com>
2020-01-24 10:01:03 -0500

    remove commented out imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.scss';
-// import uniqBy from 'lodash/uniqBy';
-// import { sortByKey } from './utils';
 import { findAny } from './utils';
 
 import planets from './planets.json';
