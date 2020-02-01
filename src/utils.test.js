a9d0eafb99839da98e98167ac3e2b0f311272798
Ryan <bednar85@gmail.com>
2020-02-01 15:39:42 -0500

    add comments to organise and clarify the utils tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -4,19 +4,29 @@ const testArray = [1, 2, 3, 4, 5];
 const testString = 'A long time ago in a galaxy far, far away....';
 
 it('includesAny works correctly', () => {
+  // works for a single item
   expect(includesAny([1], testArray)).toBeTruthy();
-  expect(includesAny([1, 2, 3], testArray)).toBeTruthy();
-  expect(includesAny([1, 6], testArray)).toBeTruthy();
   expect(includesAny([6], testArray)).toBeFalsy();
+
+  // works for a multiple items
+  expect(includesAny([1, 2, 3], testArray)).toBeTruthy();
   expect(includesAny([6, 7, 8], testArray)).toBeFalsy();
+
+  // works when one item is the haystack and another isn't
+  expect(includesAny([1, 6], testArray)).toBeTruthy();
 });
 
 it('includesOnly works correctly', () => {
+  // works for a single item
   expect(includesOnly([1], testArray)).toBeTruthy();
-  expect(includesOnly([1, 2, 3], testArray)).toBeTruthy();
-  expect(includesOnly([1, 6], testArray)).toBeFalsy();
   expect(includesOnly([6], testArray)).toBeFalsy();
+
+  // works for a multiple items
+  expect(includesOnly([1, 2, 3], testArray)).toBeTruthy();
   expect(includesOnly([6, 7, 8], testArray)).toBeFalsy();
+
+  // works when one item is the haystack and another isn't
+  expect(includesOnly([1, 6], testArray)).toBeFalsy();
 });
 
 it('search works correctly', () => {
