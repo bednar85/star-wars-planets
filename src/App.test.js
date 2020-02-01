507a679359b336e44592c3a1eff4a6f63af2ce67
Ryan <bednar85@gmail.com>
2020-02-01 18:31:26 -0500

    add a few snapshot and handler tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,53 @@
-import React from 'react';
-import { render } from '@testing-library/react';
 import App from 'App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  const defaultState = {
+    filters: {
+      media: 'All',
+      era: [],
+      myCanon: false
+    },
+    searchQuery: ''
+  };
+
+  it('renders correctly', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders correctly (detailed)', () => {
+    const wrapper = render(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('handles search query changes', () => {
+    const wrapper = mount(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.handleSearchQueryChange).toBeDefined();
+
+    const mockEvent = {
+      target: {
+        value: 'Tatooine'
+      }
+    };
+
+    instance.handleSearchQueryChange(mockEvent);
+
+    const expectedState = {
+      ...defaultState,
+      searchQuery: 'Tatooine'
+    };
+
+    expect(wrapper.state()).toEqual(expectedState);
+  });
+
+  it('handles filter changes', () => {
+    const wrapper = mount(<App />);
+    const instance = wrapper.instance();
+
+    expect(instance.handleFilterChange).toBeDefined();
+  });
 });
