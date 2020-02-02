b1f9379da5fa35539bb412e82693e0545120ec97
Ryan <bednar85@gmail.com>
2020-02-01 19:04:38 -0500

    add tests for filter change handler

--- src/App.test.js
+++ src/App.test.js
@@ -44,10 +44,84 @@ describe('App Component', () => {
     expect(wrapper.state()).toEqual(expectedState);
   });
 
+  // refactor this, it's brittle bc it relies on a specific order for checking the state changes
+  // change this to a describe with it blocks in it
   it('handles filter changes', () => {
     const wrapper = mount(<App />);
     const instance = wrapper.instance();
 
     expect(instance.handleFilterChange).toBeDefined();
+
+    let mockEvent = {
+      target: {
+        value: 'my-canon'
+      }
+    };
+
+    instance.handleFilterChange(mockEvent);
+
+    let expectedState = {
+      ...defaultState,
+      filters: {
+        ...defaultState.filters,
+        myCanon: true
+      }
+    };
+
+    expect(wrapper.state()).toEqual(expectedState);
+
+    mockEvent = {
+      target: {
+        value: 'media:Film'
+      }
+    };
+
+    instance.handleFilterChange(mockEvent);
+
+    expectedState = {
+      ...expectedState,
+      filters: {
+        ...expectedState.filters,
+        media: 'Film'
+      }
+    };
+
+    expect(wrapper.state()).toEqual(expectedState);
+
+    mockEvent = {
+      target: {
+        value: 'era:Prequel Trilogy'
+      }
+    };
+
+    instance.handleFilterChange(mockEvent);
+
+    expectedState = {
+      ...expectedState,
+      filters: {
+        ...expectedState.filters,
+        era: ['Prequel Trilogy']
+      }
+    };
+
+    expect(wrapper.state()).toEqual(expectedState);
+
+    mockEvent = {
+      target: {
+        value: 'era:Original Trilogy'
+      }
+    };
+
+    instance.handleFilterChange(mockEvent);
+
+    expectedState = {
+      ...expectedState,
+      filters: {
+        ...expectedState.filters,
+        era: ['Prequel Trilogy', 'Original Trilogy']
+      }
+    };
+
+    expect(wrapper.state()).toEqual(expectedState);
   });
 });
