f7eabdba84666c88aff9de74559d93e8eea897e0
Ryan <bednar85@gmail.com>
2020-10-14 00:48:26 -0400

    install and implement React Hook Form

--- src/App.js
+++ src/App.js
@@ -1,177 +1,139 @@
-import React, { Component } from 'react';
-import { includesAny, search } from 'utils';
-import { FILTER_KEY } from './constants';
-
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import Header from 'components/Header/Header.jsx';
 import FilterBar from 'components/FilterBar/FilterBar.jsx';
 import PlanetCards from 'components/PlanetCards/PlanetCards.jsx';
 
 import planets from 'planets.json';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      filters: {
-        media: 'All',
-        era: [],
-        myCanon: false
-      },
-      searchQuery: ''
-    };
-  }
-
-  handleSearchQueryChange = event => {
-    const { searchQuery } = this.state;
-    const { value } = event.target;
-
-    if (value !== searchQuery) {
-      this.setState({
-        searchQuery: value
-      });
-    }
+function App() {
+  const defaultValues = {
+    media: 'All',
+    era: [],
+    myCanon: false,
+    searchQuery: ''
   };
 
-  handleFilterChange = event => {
-    const { filters } = this.state;
-    const { name: filterName, value, checked } = event.target;
+  const methods = useForm({ defaultValues });
 
-    const updatedFilters = { ...filters };
+  const watchAll = methods.watch();
+  const values = methods.getValues();
 
-    if (filterName === FILTER_KEY.MY_CANON) {
-      updatedFilters.myCanon = checked;
-    } else if (filterName === FILTER_KEY.ERA) {
-      const currentEra = filters.era;
+  console.log('App');
+  console.log('  watchAll:', watchAll);
+  console.log('  values:', values);
 
-      /**
-       * if the newly selected era is already included in filter.era, remove it
-       * otherwise, append it to the end of filter.era
-       */
-      updatedFilters.era = currentEra.includes(value)
-        ? currentEra.filter(era => era !== value)
-        : [...currentEra, value];
-    } else {
-      updatedFilters[filterName] = value;
-    }
-
-    this.setState({
-      filters: updatedFilters
-    });
-  };
-
-  myCanonAppearances = planet =>
-    planet.appearances.filter(
-      ({ title }) =>
-        title !== 'Star Wars: The Clone Wars' &&
-        title !== 'Star Wars Resistance' &&
-        !title.startsWith('Episode VII') &&
-        !title.startsWith('Episode VIII') &&
-        !title.startsWith('Episode IX')
-    );
-
-  get filteredPlanets() {
-    const { filters, searchQuery } = this.state;
-
-    const searchedPlanets = searchQuery.length
-      ? planets.filter(({ name }) => search(searchQuery, name))
-      : planets;
-
-    const activeFilterKeys = Object.entries(filters).reduce(
-      (acc, [key, value]) => {
-        /**
-         * if key is media and value is not All
-         * OR
-         * if key is era and value array has a length
-         * add filter key to accumulator
-         */
-        if (
-          (key === FILTER_KEY.MEDIA && value !== 'All') ||
-          (key === FILTER_KEY.ERA && value.length)
-        ) {
-          acc.push(key);
-        }
-
-        return acc;
-      },
-      []
-    );
-
-    // first determine if myCanon was selected, if so, reduce and modify the planets array
-    const filteredPlanets = filters.myCanon
-      ? searchedPlanets.reduce((acc, planet) => {
-          const appearances = this.myCanonAppearances(planet);
-
-          if (appearances.length) {
-            acc.push({
-              ...planet,
-              appearances
-            });
-          }
-
-          return acc;
-        }, [])
-      : searchedPlanets;
-
-    // if both filters are applied check both conditions
-    if (activeFilterKeys.length) {
-      const { media: selectedMedia, era: selectedEras } = filters;
-
-      if (activeFilterKeys.length > 1) {
-        return filteredPlanets.filter(({ appearances }) => {
-          if (!appearances.length) {
-            return false;
-          }
-
-          const matchingAppearances = appearances
-            .filter(({ media, title }) =>
-              selectedMedia === 'Film (Episodes Only)'
-                ? media === 'Film' && title.startsWith('Episode')
-                : media === selectedMedia
-            )
-            .map(({ era }) => era);
-
-          return includesAny(selectedEras, matchingAppearances);
-        });
-      }
-
-      // if only one filter is applied
-      const activeFilterKey = activeFilterKeys[0];
-
-      return filteredPlanets.filter(({ appearances }) => {
-        // a map of either all of the eras or all of the media values associated with all of the appearances
-        const appearanceValues = appearances.map(
-          appearance => appearance[activeFilterKey]
-        );
-
-        if (activeFilterKey === 'era') {
-          return includesAny(selectedEras, appearanceValues);
-        }
-
-        // check if any of the film titles start with "Episode"
-        return selectedMedia === 'Film (Episodes Only)'
-          ? appearances
-              .filter(({ media }) => media === 'Film')
-              .some(({ title }) => title.startsWith('Episode'))
-          : appearanceValues.includes(selectedMedia);
-      });
-    }
-
-    return filteredPlanets;
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <FilterBar
-          searchFormHandler={this.handleSearchQueryChange}
-          filterFormHandler={this.handleFilterChange}
-        />
-        <PlanetCards planets={this.filteredPlanets} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header />
+      <FilterBar {...methods} />
+      <PlanetCards planets={planets} />
+    </div>
+  );
 }
 
 export default App;
+
+// need to refactor and work this filtering logic back in
+// import { includesAny, search } from 'utils';
+// import { FILTER_KEY } from './constants';
+
+//   myCanonAppearances = planet =>
+//     planet.appearances.filter(
+//       ({ title }) =>
+//         title !== 'Star Wars: The Clone Wars' &&
+//         title !== 'Star Wars Resistance' &&
+//         !title.startsWith('Episode VII') &&
+//         !title.startsWith('Episode VIII') &&
+//         !title.startsWith('Episode IX')
+//     );
+
+//   get filteredPlanets() {
+//     const { filters, searchQuery } = this.state;
+
+//     const searchedPlanets = searchQuery.length
+//       ? planets.filter(({ name }) => search(searchQuery, name))
+//       : planets;
+
+//     const activeFilterKeys = Object.entries(filters).reduce(
+//       (acc, [key, value]) => {
+//         /**
+//          * if key is media and value is not All
+//          * OR
+//          * if key is era and value array has a length
+//          * add filter key to accumulator
+//          */
+//         if (
+//           (key === FILTER_KEY.MEDIA && value !== 'All') ||
+//           (key === FILTER_KEY.ERA && value.length)
+//         ) {
+//           acc.push(key);
+//         }
+
+//         return acc;
+//       },
+//       []
+//     );
+
+//     // first determine if myCanon was selected, if so, reduce and modify the planets array
+//     const filteredPlanets = filters.myCanon
+//       ? searchedPlanets.reduce((acc, planet) => {
+//           const appearances = this.myCanonAppearances(planet);
+
+//           if (appearances.length) {
+//             acc.push({
+//               ...planet,
+//               appearances
+//             });
+//           }
+
+//           return acc;
+//         }, [])
+//       : searchedPlanets;
+
+//     // if both filters are applied check both conditions
+//     if (activeFilterKeys.length) {
+//       const { media: selectedMedia, era: selectedEras } = filters;
+
+//       if (activeFilterKeys.length > 1) {
+//         return filteredPlanets.filter(({ appearances }) => {
+//           if (!appearances.length) {
+//             return false;
+//           }
+
+//           const matchingAppearances = appearances
+//             .filter(({ media, title }) =>
+//               selectedMedia === 'Film (Episodes Only)'
+//                 ? media === 'Film' && title.startsWith('Episode')
+//                 : media === selectedMedia
+//             )
+//             .map(({ era }) => era);
+
+//           return includesAny(selectedEras, matchingAppearances);
+//         });
+//       }
+
+//       // if only one filter is applied
+//       const activeFilterKey = activeFilterKeys[0];
+
+//       return filteredPlanets.filter(({ appearances }) => {
+//         // a map of either all of the eras or all of the media values associated with all of the appearances
+//         const appearanceValues = appearances.map(
+//           appearance => appearance[activeFilterKey]
+//         );
+
+//         if (activeFilterKey === 'era') {
+//           return includesAny(selectedEras, appearanceValues);
+//         }
+
+//         // check if any of the film titles start with "Episode"
+//         return selectedMedia === 'Film (Episodes Only)'
+//           ? appearances
+//               .filter(({ media }) => media === 'Film')
+//               .some(({ title }) => title.startsWith('Episode'))
+//           : appearanceValues.includes(selectedMedia);
+//       });
+//     }
+
+//     return filteredPlanets;
+//   }
--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FILTER_KEY } from '../../constants';
 
-function FilterBar(props) {
-  const { filterFormHandler, searchFormHandler } = props;
-
+function FilterBar({ register }) {
   const renderInputs = inputData => {
     const { name, defaultChecked, labels, type } = inputData;
 
@@ -29,6 +27,7 @@ function FilterBar(props) {
             name={name}
             value={label}
             defaultChecked={isCheckedByDefault}
+            ref={register}
           />
           <label
             className="filter-form-label"
@@ -42,45 +41,46 @@ function FilterBar(props) {
   };
 
   return (
-    <div className="search-and-filter-forms">
-      <form className="search-form" onChange={searchFormHandler}>
-        <input type="text" className="search-form-input" />
-      </form>
-      <form className="filter-form" onChange={filterFormHandler}>
-        <fieldset className="filter-form-fieldset">
-          <h2 className="filter-form-heading">Media:</h2>
-          {renderInputs({
-            type: 'radio',
-            name: FILTER_KEY.MEDIA,
-            labels: ['All', 'Film', 'Film (Episodes Only)', 'TV Series'],
-            defaultChecked: 0
-          })}
-        </fieldset>
-        <fieldset className="filter-form-fieldset">
-          <h2 className="filter-form-heading">Era:</h2>
-          {renderInputs({
-            type: 'checkbox',
-            name: FILTER_KEY.ERA,
-            labels: ['Prequel Trilogy', 'Original Trilogy', 'Sequel Trilogy']
-          })}
-        </fieldset>
-        <fieldset className="filter-form-fieldset">
-          <h2 className="filter-form-heading">Canon:</h2>
-          <div className="filter-form-input-wrapper">
-            <input
-              type="checkbox"
-              className="filter-form-input"
-              id={FILTER_KEY.MY_CANON}
-              name={FILTER_KEY.MY_CANON}
-              value={FILTER_KEY.MY_CANON}
-            />
-            <label className="filter-form-label" htmlFor={FILTER_KEY.MY_CANON}>
-              My Canon
-            </label>
-          </div>
-        </fieldset>
-      </form>
-    </div>
+    <form className="filter-form">
+      <input
+        type="text"
+        name="searchQuery"
+        className="search-form-input"
+        ref={register}
+      />
+      <fieldset className="filter-form-fieldset">
+        <h2 className="filter-form-heading">Media:</h2>
+        {renderInputs({
+          type: 'radio',
+          name: FILTER_KEY.MEDIA,
+          labels: ['All', 'Film', 'Film (Episodes Only)', 'TV Series'],
+          defaultChecked: 0
+        })}
+      </fieldset>
+      <fieldset className="filter-form-fieldset">
+        <h2 className="filter-form-heading">Era:</h2>
+        {renderInputs({
+          type: 'checkbox',
+          name: FILTER_KEY.ERA,
+          labels: ['Prequel Trilogy', 'Original Trilogy', 'Sequel Trilogy']
+        })}
+      </fieldset>
+      <fieldset className="filter-form-fieldset">
+        <h2 className="filter-form-heading">Canon:</h2>
+        <div className="filter-form-input-wrapper">
+          <input
+            type="checkbox"
+            className="filter-form-input"
+            id={FILTER_KEY.MY_CANON}
+            name={FILTER_KEY.MY_CANON}
+            ref={register}
+          />
+          <label className="filter-form-label" htmlFor={FILTER_KEY.MY_CANON}>
+            My Canon
+          </label>
+        </div>
+      </fieldset>
+    </form>
   );
 }
 
