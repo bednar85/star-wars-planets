bcb31adadc29a5e14e3c8f613ebb4d91ac86a39a
Ryan <bednar85@gmail.com>
2020-10-19 20:58:15 -0400

    Updates

---
+++ precache-manifest.088767a81186540dc4e9c3b94089b5db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d3dbe5792bb1a0e837f0260d0c7923",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "ec3e30ed3bad40e7f313",
+    "url": "/star-wars-planets/static/css/main.4b16c0ad.chunk.css"
+  },
+  {
+    "revision": "bf2c45d0481ac75771fc",
+    "url": "/star-wars-planets/static/js/2.44e8cbe5.chunk.js"
+  },
+  {
+    "revision": "8e44bf96742e4a009440b7e9866ff144",
+    "url": "/star-wars-planets/static/js/2.44e8cbe5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec3e30ed3bad40e7f313",
+    "url": "/star-wars-planets/static/js/main.81f3bc2e.chunk.js"
+  },
+  {
+    "revision": "872288d1b88b38025f08",
+    "url": "/star-wars-planets/static/js/runtime-main.2767ba3d.js"
+  },
+  {
+    "revision": "6980b4427bfb68bb1191542bd33a57ce",
+    "url": "/star-wars-planets/static/media/outage-webfont.6980b442.woff"
+  },
+  {
+    "revision": "699ede79ecd7159e3da3d5076b5bb117",
+    "url": "/star-wars-planets/static/media/outage-webfont.699ede79.woff2"
+  },
+  {
+    "revision": "a1433d6522143e1d3c3af23b8e1a366b",
+    "url": "/star-wars-planets/static/media/rostyslav-zagornov-92gooayayc4.a1433d65.jpg"
+  },
+  {
+    "revision": "1a90f3efbd46e5e2c8a9b6189b8d96c4",
+    "url": "/star-wars-planets/static/media/search.1a90f3ef.svg"
+  },
+  {
+    "revision": "9b7050e567502d54b439c5e06411b08b",
+    "url": "/star-wars-planets/static/media/star-wars-logo-yellow.9b7050e5.png"
+  }
+]);
